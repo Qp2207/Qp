19ff407064c2137e737ca892e6eff4eab99ca394
Qp2207 <quanphan22072007@gmail.com>
2026-05-27 21:13:09 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,27 @@
+const products = [
+
+  {
+    id: 1,
+    code: 'AO-001',
+    image: 'images/AO-001.jpg',
+    link: 'https://shopee.vn/link1',
+    cat: 'ao'
+  },
+
+  {
+    id: 2,
+    code: 'AO-002',
+    image: 'images/AO-002.jpg',
+    link: 'https://shopee.vn/link2',
+    cat: 'ao'
+  },
+
+  {
+    id: 3,
+    code: 'QU-001',
+    image: 'images/QU-001.jpg',
+    link: 'https://shopee.vn/link3',
+    cat: 'quan'
+  }
+
+];
