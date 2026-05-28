3679d2b71fc506796a91025e12fada3022fec75d
Qp2207 <quanphan22072007@gmail.com>
2026-05-28 21:09:09 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,27 +1,51 @@
 const products = [
 
   {
-    id: 1,
-    code: 'AO-001',
-    image: 'images/AO-001.jpg',
-    link: 'https://shopee.vn/link1',
-    cat: 'ao'
-  },
+  id: 1,
+  code: 'AO-001',
+  image: 'images/AO-001.jpg',
+  link: 'https://vt.tiktok.com/ZS9YocRRe1Dfy-3G178/',
+  cat: 'ao'
+},
 
-  {
-    id: 2,
-    code: 'AO-002',
-    image: 'images/AO-002.jpg',
-    link: 'https://shopee.vn/link2',
-    cat: 'ao'
-  },
+{
+  id: 2,
+  code: 'AO-002',
+  image: 'images/AO-002.jpg',
+  link: 'https://vt.tiktok.com/ZS9YocNA49mYG-SgKir/',
+  cat: 'ao'
+},
 
-  {
-    id: 3,
-    code: 'QU-001',
-    image: 'images/QU-001.jpg',
-    link: 'https://shopee.vn/link3',
-    cat: 'quan'
-  }
+{
+  id: 4,
+  code: 'AO-004',
+  image: 'images/AO-004.jpg',
+  link: 'https://vt.tiktok.com/ZS9YocNbrHmgt-CYRLl/',
+  cat: 'ao'
+},
+
+{
+  id: 7,
+  code: 'AO-007',
+  image: 'images/AO-007.jpg',
+  link: 'https://vt.tiktok.com/ZS9Yoc2svjXJd-Sr1DH/',
+  cat: 'ao'
+},
+
+{
+  id: 5,
+  code: 'AO-005',
+  image: 'images/AO-005.jpg',
+  link: 'https://vt.tiktok.com/ZS9Yocr58k5Mm-SBvDb/',
+  cat: 'ao'
+},
+
+{
+  id: 6,
+  code: 'AO-006',
+  image: 'images/AO-006.jpg',
+  link: 'https://vt.tiktok.com/ZS9YockCokaHd-dFsvJ/',
+  cat: 'ao'
+}
 
 ];
