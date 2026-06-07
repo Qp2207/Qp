7ffc1d0c031d49cbd11f60a6e949020d932e877a
Qp2207 <quanphan22072007@gmail.com>
2026-06-07 21:04:01 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,56 +1,16 @@
 const products = [
 
-  {
+{
   id: 1,
-  code: 'AO-001',
-  image: 'images/AO-001.jpg',
-  link: 'https://vt.tiktok.com/ZS9YocRRe1Dfy-3G178/',
-  cat: 'ao'
-},
-
-{
-  id: 2,
-  code: 'AO-002',
-  image: 'images/AO-002.jpg',
-  link: 'https://vt.tiktok.com/ZS9YocNA49mYG-SgKir/',
-  cat: 'ao'
-},
-
-{
-  id: 4,
-  code: 'AO-004',
-  image: 'images/AO-004.jpg',
-  link: 'https://vt.tiktok.com/ZS9YocNbrHmgt-CYRLl/',
-  cat: 'ao'
-},
-
-{
-  id: 7,
-  code: 'AO-007',
-  image: 'images/AO-007.jpg',
-  link: 'https://vt.tiktok.com/ZS9Yoc2svjXJd-Sr1DH/',
-  cat: 'ao'
-},
-
-{
-  id: 5,
-  code: 'AO-005',
-  image: 'images/AO-005.jpg',
-  link: 'https://vt.tiktok.com/ZS9Yocr58k5Mm-SBvDb/',
-  cat: 'ao'
-},
-
-{
-  id: 6,
-  code: 'AO-006',
-  image: 'images/AO-006.jpg',
+  code: 'AV001',
+  image: 'images/AV001.jpg',
   link: 'https://vt.tiktok.com/ZS9YockCokaHd-dFsvJ/',
   cat: 'ao'
 },
 {
-  id: 8,
-  code: 'AO-008',
-  image: 'images/AO-008.jpg',
+  id: 2,
+  code: 'AV002',
+  image: 'images/AV002.jpg',
   link: 'https://vt.tiktok.com/ZS92nJaQsyS3m-ygXcY/',
   cat: 'ao'
 }
