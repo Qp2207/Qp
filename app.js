cd70749472402350bc3cfb7b8a84bb996f80cb26
Qp2207 <quanphan22072007@gmail.com>
2026-09-07 21:13:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // Sau khi đưa words.json lên GitHub, thay URL dưới đây bằng:
 // https://raw.githubusercontent.com/USERNAME/REPOSITORY/main/words.json
 const GITHUB_DATA_URL =
-  "https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPOSITORY/main/words.json";
+  "https://raw.githubusercontent.com/qp2207/Qp/main/words.json";
 
 let lessons = [];
 let selectedLesson = null;
